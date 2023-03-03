ebc4a45d4613161b122aa74887c577eb10aed7c0
md muhaiminul <muhai4959@gmail.com>
2023-03-03 22:19:55 +0600

    fix some  issues and bugs

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//sort and store api data array
+//sort and store api data in array
 let apiData = [];
 // fetch api and get the data 
 const loadData = (length) =>{
@@ -8,7 +8,7 @@ const loadData = (length) =>{
     .then(data => {
         apiData = data.data.tools;
         displayData(apiData, length);
-     });
+    });
 }
 //display data for each card
 const displayData = (data, length) =>{
@@ -28,14 +28,14 @@ const displayData = (data, length) =>{
     data.forEach(feature => {
         // console.log(feature);
         const div = document.createElement("div");
-        div.classList.add("border", "p-5", "rounded-xl", "space-y-3", "shadow-lg");
+        div.classList.add("border", "p-5", "rounded-xl", "space-y-3", "shadow-xl");
         div.innerHTML = `
                 <img src="${feature.image}" class="rounded-xl h-64">
                 <h3 class="text-xl font-bold">Features</h3>
                 <ol class="list-decimal list-inside text-slate-700 leading-loose">
-                    <li>${feature.features[0]}</li>
-                    <li>${feature.features[1]}</li>
-                    <li>${feature.features[2] ? feature.features[2]: "No available data found"}</li>
+                    <li>${feature.features[0] ? feature.features[0] : "No data found"}</li>
+                    <li>${feature.features[1] ? feature.features[1] : "No data found"}</li>
+                    <li>${feature.features[2] ? feature.features[2] : "No data found"}</li>
                 </ol>
                 <hr class="border">
                 <div class="flex justify-between items-center">
@@ -99,9 +99,9 @@ const modalDetails = cardData =>{
             <div class="border border-red-400 p-5 bg-red-50 space-y-5 rounded-xl w-full shadow-xl">
                 <p class="text-xl font-bold">${cardData.description}</P>
                 <div class="flex gap-3 font-bold md:flex-row flex-col">
-                    <p class="text-green-700 p-5 bg-white rounded-md w-full">${cardData.pricing ? cardData.pricing[0].price : "No Data Found"}  <br> ${cardData.pricing ? cardData.pricing[0].plan : "No Data Found"}</p>
-                    <p class="text-orange-500 p-5 bg-white rounded-md w-full">${cardData.pricing ? cardData.pricing[1].price : "No Data Found"}  <br> ${cardData.pricing ? cardData.pricing[1].plan : "No Data Found"}</p>
-                    <p class="text-pink-700 p-5 bg-white rounded-md w-full">${cardData.pricing ? cardData.pricing[2].price : "No Data Found"}  <br> ${cardData.pricing ? cardData.pricing[2].plan : "No Data Found"}</p>
+                    <p class="text-green-700 p-5 bg-white rounded-md w-full">${cardData.pricing ? (cardData.pricing[0].price === "0" ? "Free of Cost" : cardData.pricing[0].price ) : "Free of Cost"}  <br> ${cardData.pricing ? cardData.pricing[0].plan : "Basic"}</p>
+                    <p class="text-orange-500 p-5 bg-white rounded-md w-full">${cardData.pricing ? cardData.pricing[1].price : "Free of Cost"}  <br> ${cardData.pricing ? cardData.pricing[1].plan : "Pro"}</p>
+                    <p class="text-pink-700 p-5 bg-white rounded-md w-full">${cardData.pricing ? cardData.pricing[2].price : "Free of Cost"}  <br> ${cardData.pricing ? cardData.pricing[2].plan : "Enterprise"}</p>
                 </div>
                 <div class="flex gap-5 justify-between md:flex-row flex-col">
                     <div>
@@ -114,24 +114,35 @@ const modalDetails = cardData =>{
                     </div>
                     <div>
                         <p class="text-xl font-bold mb-2">Integrations</p>
-                        <ul class="list-disc list-inside text-gray-500 space-y-2">
-                            <li>${cardData.integrations ? (cardData.integrations[0] ? cardData.integrations[0] : "No Data Found") : "No Data Found"}</li>
-                            <li>${cardData.integrations ? (cardData.integrations[1] ? cardData.integrations[1] : "No Data Found") : "No Data Found"}</li>
-                            <li>${cardData.integrations ? (cardData.integrations[2] ? cardData.integrations[2] : "No Data Found" ) : "No Data Found"}</li> 
+                        <ul class="list-disc list-inside text-gray-500 space-y-2" id="${cardData.id*15}">
+                             
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="border md:p-5 rounded-xl text-center relative w-full shadow-xl">
                 <button class="bg-red-400 text-white md:p-2 p-px rounded-md md:font-semibold absolute md:top-6 top-0 md:right-6 right-0 hidden" id="${cardData.id}">${cardData.accuracy.score*100}% accuracy</button>
-                <img src="${cardData.image_link[0]}" class="rounded-xl mb-5 w-full">
-                <p class="text-xl font-bold mb-5">${cardData.input_output_examples ? cardData.input_output_examples[0].input : "No Data Found"}</p>
-                <p class="text-gray-500">${cardData.input_output_examples ? cardData.input_output_examples[0].output : "No Data Found"}</p>
+                <img src="${cardData.image_link[0]}" class="rounded-xl mb-5">
+                <p class="text-xl font-bold mb-5">${cardData.input_output_examples ? cardData.input_output_examples[0].input : "Can you give any example?"}</p>
+                <p class="text-gray-500 p-2">${cardData.input_output_examples ? cardData.input_output_examples[0].output : "No! Not Yet! Take a break!!!"}</p>
             </div>
         </div>
     `;
+    //set integrations item if value is null then return not found otherwise return it
+    const integrationsID = document.getElementById(`${cardData.id*15}`);
+    if(cardData.integrations == null){
+        const li = document.createElement("li");
+        li.innerText = "No data found";
+        integrationsID.appendChild(li);
+    } else {
+        cardData.integrations.forEach(data => {
+            const li = document.createElement("li");
+            li.innerText = `${data}`;
+            integrationsID.appendChild(li);
+        })
+    }
+    //if score value is null then wont show button otherwise show it
     const score = cardData.accuracy.score;
-    // console.log(score);
     const imgContainer = document.getElementById(`${cardData.id}`);
     if(cardData.accuracy.score == null){
         imgContainer.classList.add("hidden");
